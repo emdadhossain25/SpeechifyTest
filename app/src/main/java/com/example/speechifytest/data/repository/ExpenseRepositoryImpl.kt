74d2e849ff11c6905994e3cd30e447ebfb62208b
Emdad Hossain <emdadhossain25@users.noreply.github.com>
2025-11-02 01:02:56 +0600

    repository implementation done

--- app/src/main/java/com/example/speechifytest/data/repository/ExpenseRepositoryImpl.kt
+++ app/src/main/java/com/example/speechifytest/data/repository/ExpenseRepositoryImpl.kt
@@ -1,8 +1,32 @@
 package com.example.speechifytest.data.repository
 
+import com.example.speechifytest.data.model.Expense
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class ExpenseRepositoryImpl @Inject constructor(): IExpenseRepository{
+class ExpenseRepositoryImpl @Inject constructor() : IExpenseRepository {
+
+    val expensesList: MutableList<Expense> = emptyList<Expense>() as MutableList<Expense>
+    override suspend fun addExpense(expense: Expense): Result<Unit> {
+        try {
+            expensesList.add(expense)
+            return Result.success(Unit)
+        } catch (e: Exception) {
+            return Result.failure(e)
+        }
+    }
+
+    override suspend fun getAllExpenses(): Result<List<Expense>> {
+        return try {
+            Result.success(expensesList)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+//    override fun getExpensesFlow(): Flow<Expense>= falselow {
+//        emit()
+//    }
 }
--- app/src/main/java/com/example/speechifytest/data/repository/IExpenseRepository.kt
+++ app/src/main/java/com/example/speechifytest/data/repository/IExpenseRepository.kt
@@ -1,4 +1,11 @@
 package com.example.speechifytest.data.repository
 
+import com.example.speechifytest.data.model.Expense
+import kotlinx.coroutines.flow.Flow
+
 interface IExpenseRepository {
+
+    suspend fun addExpense(expense: Expense): Result<Unit>
+    suspend fun getAllExpenses(): Result<List<Expense>>
+//    fun getExpensesFlow(): Flow<Expense>
 }
