d659d3548e85d83a7a785d4570a9f0912d4faf6d
Emdad Hossain <emdadhossain25@users.noreply.github.com>
2025-11-02 00:11:13 +0600

    removed theme from MainActivity class to avoid error

--- app/src/main/java/com/example/speechifytest/MainActivity.kt
+++ app/src/main/java/com/example/speechifytest/MainActivity.kt
@@ -3,20 +3,12 @@ package com.example.speechifytest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
-import com.example.speechifytest.ui.theme.SpeechifyTestTheme
-import dagger.hilt.android.AndroidEntryPoint
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            SpeechifyTestTheme {
-            }
+
         }
     }
 }
